43cbe70d171d3169abacd88c69c5307441cfb5e8
KetlenEmanuelly <ketlen.emanuelly@aluno.educa.go.gov.br>
2026-09-03 17:54:53 -0300

    1005

---
+++ 1005.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double A = double.Parse(Console.ReadLine());
+        double B = double.Parse(Console.ReadLine());
+
+        double MEDIA = (A * 3.5 + B * 7.5) / 11;
+
+        Console.WriteLine($"MEDIA = {MEDIA:F5}");
+    }
+}
